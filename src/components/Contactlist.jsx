87af53e7b6995c42dfabe18828597eaf8a82511a
AJ Williams <Ajwilliamsofficial@gmail.com>
2023-10-12 12:59:48 -0700

    Selected Contact View WIP

--- src/components/Contactlist.jsx
+++ src/components/Contactlist.jsx
@@ -7,19 +7,21 @@ const dummyContacts = [
     { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
   ];
-export default function Contactlist() {
+export default function Contactlist({ setSelectedContactId }) {
     const [contacts, setContacts] = useState(dummyContacts);
+
+    
     useEffect (() => {
         async function fetchContacts() {
             try {
-                const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/');
-                const data = await response.json();
-            }
-            catch (error) {
+                const response = await fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users');
+                const result = await response.json();
+                setContacts(result);
+            } catch (error) {
                 console.error(error);
             }
         } 
-        fetchContacts()
+        fetchContacts();
     }, []);
     
 
